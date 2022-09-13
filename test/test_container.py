9b66ba5f1e1c8001aa3cb2d9756a3431c130c076
saranya <nukai@amazon.com>
2022-09-13 12:41:45 -0700

    feat: add container management

---
+++ test/test_container.py
@@ -0,0 +1,38 @@
+from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
+from awsiot.greengrasscoreipc.model import ConfigurationUpdateEvents, SubscribeToConfigurationUpdateResponse
+from src.configuration_handler import ComponentConfigurationIPCHandler
+from src.container import ContainerManagement
+
+
+def test_container_management_init(mocker):
+    mocker.patch("awsiot.greengrasscoreipc", return_value=None)
+    mocker.patch("src.configuration_handler", return_value=None)
+    mock_ipc_client = GreengrassCoreIPCClientV2()
+    mock_configuration_handler = ComponentConfigurationIPCHandler(mock_ipc_client)
+    mock_subscribe_config = mocker.patch.object(mock_ipc_client, "subscribe_to_configuration_update", return_value=None)
+    ContainerManagement(mock_ipc_client, mock_configuration_handler)
+
+    assert mock_subscribe_config.call_count == 1
+
+
+def test_container_management_update_event(mocker):
+    mocker.patch("awsiot.greengrasscoreipc", return_value=None)
+    mocker.patch("src.configuration_handler", return_value=None)
+    mock_ipc_client = GreengrassCoreIPCClientV2()
+    mock_configuration_handler = ComponentConfigurationIPCHandler(mock_ipc_client)
+    mock__on_configuration_update_event = mocker.patch.object(
+        ContainerManagement, "_on_configuration_update_event", return_value=None
+    )
+
+    def this_triggers_callbacks(*args, **kwargs):
+        config_update_events = ConfigurationUpdateEvents()
+        kwargs["on_stream_event"](config_update_events)
+        return SubscribeToConfigurationUpdateResponse()
+
+    mock_subscribe_config = mocker.patch.object(
+        mock_ipc_client, "subscribe_to_configuration_update", side_effect=this_triggers_callbacks
+    )
+    ContainerManagement(mock_ipc_client, mock_configuration_handler)
+
+    assert mock_subscribe_config.call_count == 1
+    assert mock__on_configuration_update_event.call_count == 1
