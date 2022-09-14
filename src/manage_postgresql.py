9aad38c79c5a4ca9873e886283c8071a50b84a57
saranya <nukai@amazon.com>
2022-09-14 12:24:04 -0700

    feat: use docker client for container lifecycle

--- src/manage_postgresql.py
+++ src/manage_postgresql.py
@@ -1,11 +1,20 @@
+import logging
+import sys
+
+import docker
 from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
 
 from src.configuration_handler import ComponentConfigurationIPCHandler
 from src.container import ContainerManagement
 
 if __name__ == "__main__":
+    # Configure logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
     ipc_client = GreengrassCoreIPCClientV2()
+    docker_client = docker.DockerClient(version="auto")
     configuration_handler = ComponentConfigurationIPCHandler(ipc_client)
-    container_management = ContainerManagement(ipc_client)
+    container_management = ContainerManagement(ipc_client, docker_client, configuration_handler)
     container_management.manage_postgresql_container(configuration_handler.get_configuration())
-    # Will the start the docker container with the latest component configuration
--- test/test_container.py
+++ test/test_container.py
@@ -1,5 +1,11 @@
 from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
-from awsiot.greengrasscoreipc.model import ConfigurationUpdateEvents, SubscribeToConfigurationUpdateResponse
+from awsiot.greengrasscoreipc.model import (
+    ConfigurationUpdateEvent,
+    ConfigurationUpdateEvents,
+    GetConfigurationResponse,
+    SubscribeToConfigurationUpdateResponse,
+)
+from docker.models.containers import Container
 from src.configuration_handler import ComponentConfigurationIPCHandler
 from src.container import ContainerManagement
 
@@ -18,4 +24,87 @@ def test_container_management_update_event(mocker):
     mocker.patch.object(mock_ipc_client, "subscribe_to_configuration_update", side_effect=this_triggers_callbacks)
     mocker.patch.object(ContainerManagement, "manage_postgresql_container", return_value=None)
 
-    ContainerManagement(mock_ipc_client, mock_configuration_handler)
+    ContainerManagement(mock_ipc_client, None, mock_configuration_handler)
+
+
+def test_container_management_create_container_if_it_does_not_exist(mocker):
+    mocker.patch("awsiot.greengrasscoreipc", return_value=None)
+    mocker.patch("src.configuration_handler", return_value=None)
+    mock_ipc_client = GreengrassCoreIPCClientV2()
+    mock_configuration_handler = ComponentConfigurationIPCHandler(mock_ipc_client)
+
+    def this_triggers_callbacks(*args, **kwargs):
+        config_update_events = ConfigurationUpdateEvents(
+            configuration_update_event=ConfigurationUpdateEvent(component_name="", key_path="/some/path")
+        )
+        kwargs["on_stream_event"](config_update_events)
+        return SubscribeToConfigurationUpdateResponse()
+
+    mocker.patch.object(mock_ipc_client, "subscribe_to_configuration_update", side_effect=this_triggers_callbacks)
+    mock_get_configuration_response = GetConfigurationResponse(
+        value={
+            "ContainerMapping": {
+                "HostPort": "8000",
+                "HostVolume": "/some/volume/",
+                "ContainerName": "some-container-name",
+                "DBCredentialSecret": "secret",
+            }
+        }
+    )
+    mocker.patch.object(GreengrassCoreIPCClientV2, "get_configuration", return_value=mock_get_configuration_response)
+    mock_stop_container = mocker.patch.object(ContainerManagement, "stop_container", return_value=None)
+    mock_run_container = mocker.patch.object(ContainerManagement, "run_container", return_value=None)
+    mock_remove_container = mocker.patch.object(ContainerManagement, "remove_container", return_value=None)
+
+    ContainerManagement(mock_ipc_client, None, mock_configuration_handler)
+
+    assert not mock_remove_container.called
+    assert not mock_stop_container.called
+
+    assert mock_run_container.called
+
+
+def test_container_management_recreate_container_if_it_exists(mocker):
+    mocker.patch("awsiot.greengrasscoreipc", return_value=None)
+    mocker.patch("src.configuration_handler", return_value=None)
+    mock_ipc_client = GreengrassCoreIPCClientV2()
+    mock_configuration_handler = ComponentConfigurationIPCHandler(mock_ipc_client)
+
+    def this_triggers_callbacks(*args, **kwargs):
+        config_update_events = ConfigurationUpdateEvents(
+            configuration_update_event=ConfigurationUpdateEvent(component_name="", key_path="/some/path")
+        )
+        kwargs["on_stream_event"](config_update_events)
+        return SubscribeToConfigurationUpdateResponse()
+
+    mocker.patch.object(mock_ipc_client, "subscribe_to_configuration_update", side_effect=this_triggers_callbacks)
+    mock_get_configuration_response = GetConfigurationResponse(
+        value={
+            "ContainerMapping": {
+                "HostPort": "8000",
+                "HostVolume": "/some/volume/",
+                "ContainerName": "some-container-name",
+                "DBCredentialSecret": "secret",
+            }
+        }
+    )
+
+    mocker.patch.object(GreengrassCoreIPCClientV2, "get_configuration", return_value=mock_get_configuration_response)
+    mock_container_object = Container()
+    mock_stop_container = mocker.patch.object(ContainerManagement, "stop_container", return_value=None)
+    mock_run_container = mocker.patch.object(ContainerManagement, "run_container", return_value=mock_container_object)
+    mock_remove_container = mocker.patch.object(ContainerManagement, "remove_container", return_value=None)
+
+    cm = ContainerManagement(mock_ipc_client, None, mock_configuration_handler)
+
+    assert not mock_remove_container.called
+    assert not mock_stop_container.called
+
+    assert mock_run_container.called
+    cm.postgresql_container = mock_container_object
+    cm.manage_postgresql_container(mock_configuration_handler.get_configuration())
+
+    assert mock_remove_container.called
+    assert mock_stop_container.called
+
+    assert mock_run_container.called
