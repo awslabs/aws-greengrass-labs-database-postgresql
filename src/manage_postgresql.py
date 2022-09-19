ecc2e554a4404081e65d06d55f00dcaa82d1e1c0
Saranya <nukai@amazon.com>
2022-09-19 15:56:18 -0700

    feat: add shutdown lifecycle and refactor (#9)

--- src/manage_postgresql.py
+++ src/manage_postgresql.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     docker_client = docker.DockerClient(version="auto")
     configuration_handler = ComponentConfigurationIPCHandler(ipc_client)
     container_management = ContainerManagement(ipc_client, docker_client, configuration_handler)
+    container_management.subscribe_to_configuration_updates()
     container_management.manage_postgresql_container(configuration_handler.get_configuration())
     # Keep the main thread alive to listen for component updates
     while True:
---
+++ src/shutdown_component.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+
+import docker
+from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
+
+from src.configuration_handler import ComponentConfigurationIPCHandler
+
+
+def configure_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def cleanup_container(docker_client, container_name):
+    try:
+        postgresql_container = docker_client.containers.get(container_name)
+        if postgresql_container:
+            postgresql_container.stop()
+            postgresql_container.remove()
+    except Exception:
+        logging.exception("Exception occurred while removing the container")
+
+
+def main():
+    configure_logging()
+    ipc_client = GreengrassCoreIPCClientV2()
+    configuration_handler = ComponentConfigurationIPCHandler(ipc_client)
+    container_name = configuration_handler.get_configuration().get_container_name()
+    docker_client = docker.DockerClient(version="auto")
+    cleanup_container(docker_client, container_name)
+
+if __name__ == "__main__":
+    main()
