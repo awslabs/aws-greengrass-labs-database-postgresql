03261ed60d9776d15f9c3603b4ba74813967c574
Saranya <nukai@amazon.com>
2022-09-22 12:45:45 -0700

    refactor: improve logging, remove data volume overlap, pass passwords as file (#14)

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 HOST_PORT_KEY = "HostPort"
 HOST_VOLUME_KEY = "HostVolume"
@@ -7,13 +7,17 @@ CONTAINER_MAPPING_KEY = "ContainerMapping"
 DB_CREDENTIAL_SECRET_KEY = "DBCredentialSecret"
 POSTGRES_USERNAME_KEY = "POSTGRES_USER"
 POSTGRES_PASSWORD_KEY = "POSTGRES_PASSWORD"
+POSTGRES_USERNAME_FILE_KEY = "POSTGRES_USER_FILE"
+POSTGRES_PASSWORD_FILE_KEY = "POSTGRES_PASSWORD_FILE"
 DEFAULT_HOST_PORT = "5432"
 DEFAULT_CONTAINER_NAME = "greengrass_postgresql"
-DEFAULT_HOST_VOLUME = f"{os.getcwd()}/postgresql"
+DEFAULT_HOST_VOLUME = Path().joinpath("postgresql").resolve()
 DEFAULT_CONTAINER_PORT = "5432/tcp"
 DEFAULT_DB_NAME = "postgres"
 POSTGRES_DB_KEY = "POSTGRES_DB"
 DEFAULT_CONTAINER_VOLUME = "/var/lib/postgresql/data"
 POSTGRES_IMAGE = "postgres:alpine3.16"
 POSTGRES_SERVER_CONFIGURATION_FILES_KEY = "ConfigurationFiles"
-SUPPORTED_CONFIGURATION_FILES = ["postgresql.conf", "pg_hba.conf", "pg_ident.conf"]
+SUPPORTED_CONFIGURATION_FILES = {"postgresql.conf": "config_file", "pg_hba.conf": "hba_file", "pg_ident.conf": "ident_file"}
+CUSTOM_FILES = "/custom_files"
+SECRETS_KEY = "secrets"
--- src/container.py
+++ src/container.py
@@ -1,6 +1,8 @@
 import logging
+from pathlib import Path
 from threading import Lock, Thread
 
+import docker.errors
 from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
 from awsiot.greengrasscoreipc.model import ConfigurationUpdateEvents
 from docker.models.containers import Container
@@ -8,14 +10,16 @@ from docker.models.containers import Container
 from src.configuration import ComponentConfiguration
 from src.configuration_handler import ComponentConfigurationIPCHandler
 from src.constants import (
-    DB_CREDENTIAL_SECRET_KEY,
+    CUSTOM_FILES,
     DEFAULT_CONTAINER_PORT,
     DEFAULT_CONTAINER_VOLUME,
     DEFAULT_DB_NAME,
     POSTGRES_DB_KEY,
     POSTGRES_IMAGE,
-    POSTGRES_PASSWORD_KEY,
-    POSTGRES_USERNAME_KEY,
+    POSTGRES_PASSWORD_FILE_KEY,
+    POSTGRES_USERNAME_FILE_KEY,
+    SECRETS_KEY,
+    SUPPORTED_CONFIGURATION_FILES,
 )
 
 
@@ -29,6 +33,7 @@ class ContainerManagement:
         self.postgresql_container = None
         self.lock = Lock()
         self.current_configuration = config_handler.get_configuration()
+        self.secrets_path = Path().joinpath(SECRETS_KEY).resolve()
 
     def subscribe_to_configuration_updates(self):
         """
@@ -58,23 +63,24 @@ class ContainerManagement:
     def _on_configuration_update_event(self, events: ConfigurationUpdateEvents):
         if not events.configuration_update_event:
             return
-        key_path = events.configuration_update_event.key_path
-        if DB_CREDENTIAL_SECRET_KEY not in key_path:
-            with self.lock:
-                component_configuration = self.config_handler.get_configuration()
-                if self.current_configuration != component_configuration:
-                    self.current_configuration = component_configuration
-                    self.manage_postgresql_container(component_configuration)
+        with self.lock:
+            component_configuration = self.config_handler.get_configuration()
+            if self.current_configuration != component_configuration:
+                self.current_configuration = component_configuration
+                self.manage_postgresql_container(component_configuration)
 
     def _set_container(self, configuration: ComponentConfiguration):
         if not self.postgresql_container:
             try:
                 self.postgresql_container = self.docker_client.containers.get(configuration.get_container_name())
+            except docker.errors.NotFound as not_found:
+                logging.debug("Exception while getting the container: ", not_found.explanation)
             except Exception as exception:
                 logging.exception(exception, exc_info=True)
 
     def manage_postgresql_container(self, configuration: ComponentConfiguration):
         self._set_container(configuration)
+        logging.info("Creating a new docker container: %s as the configuration changed", configuration.get_container_name())
         self._recreate_container(configuration)
 
     def _recreate_container(self, configuration):
@@ -86,37 +92,62 @@ class ContainerManagement:
     def _stop_container(self):
         if not self.postgresql_container:
             return
-        logging.info("Stopping the docker container : %s", self.postgresql_container.name)
-        self.postgresql_container.stop()
+        logging.info(
+            "Stopping the docker container : {}-{}".format(self.postgresql_container.name, self.postgresql_container.id)
+        )
+        try:
+            self.postgresql_container.stop()
+        except docker.errors.NotFound as e:
+            logging.debug(
+                "Could not stop the container: {}-{} as it does not exist : {}".format(
+                    self.postgresql_container.name, self.postgresql_container.id, e.explanation
+                )
+            )
 
     def _remove_container(self):
         if not self.postgresql_container:
             return
-        logging.info("Removing the docker container : %s", self.postgresql_container.name)
-        self.postgresql_container.remove()
+        logging.info(
+            "Removing the docker container : {}-{}".format(self.postgresql_container.name, self.postgresql_container.id)
+        )
+        try:
+            self.postgresql_container.remove()
+        except docker.errors.NotFound as e:
+            logging.debug(
+                "Could not remove the container: {}-{}  as it does not exist : {}".format(
+                    self.postgresql_container.name, self.postgresql_container.id, e.explanation
+                )
+            )
 
     def _get_volumes(self, config: ComponentConfiguration):
-        volumes = [f"{config.get_host_volume()}:{DEFAULT_CONTAINER_VOLUME}"]
+        volumes = [
+            f"{config.get_host_volume()}:{DEFAULT_CONTAINER_VOLUME}",
+            f"{self.secrets_path}:{CUSTOM_FILES}/{SECRETS_KEY}",
+        ]
+
         server_configuration_files = config.get_pg_config_files()
         if not server_configuration_files:
             return volumes
         for conf_file, file_abs_path in server_configuration_files.items():
-            volumes.append(f"{file_abs_path}:{DEFAULT_CONTAINER_VOLUME}/{conf_file}")
+            volumes.append(f"{file_abs_path}:{CUSTOM_FILES}/{conf_file}")
         return volumes
 
     def _run_container(self, config: ComponentConfiguration):
         db_username, db_password = config.get_db_credentials()
+        self._write_secrets_to_file(db_username, db_password)
         postgres_env = {
-            POSTGRES_USERNAME_KEY: db_username,
-            POSTGRES_PASSWORD_KEY: db_password,
+            POSTGRES_USERNAME_FILE_KEY: f"{CUSTOM_FILES}/{SECRETS_KEY}/{POSTGRES_USERNAME_FILE_KEY}",
+            POSTGRES_PASSWORD_FILE_KEY: f"{CUSTOM_FILES}/{SECRETS_KEY}/{POSTGRES_PASSWORD_FILE_KEY}",
             POSTGRES_DB_KEY: DEFAULT_DB_NAME,
         }
 
         postgres_ports = {DEFAULT_CONTAINER_PORT: config.get_host_port()}
         container_name = config.get_container_name()
         logging.info("Running the docker container : %s", container_name)
+
         self.postgresql_container = self.docker_client.containers.run(
             POSTGRES_IMAGE,
+            "{} {}".format(DEFAULT_DB_NAME, self._create_config_command(config)),
             name=container_name,
             ports=postgres_ports,
             environment=postgres_env,
@@ -125,12 +156,39 @@ class ContainerManagement:
         )
         self._follow_container_logs()
 
+    def _write_secrets_to_file(self, db_username, db_password):
+        db_user_path = self.secrets_path.joinpath(POSTGRES_USERNAME_FILE_KEY).resolve()
+        db_password_path_ = self.secrets_path.joinpath(POSTGRES_PASSWORD_FILE_KEY).resolve()
+        try:
+            # Create secrets directory if it
+            self.secrets_path.mkdir(parents=True, exist_ok=True)
+            # Write secret files
+            with open(db_user_path, "w") as u_file:
+                u_file.write(db_username)
+            with open(db_password_path_, "w") as p_file:
+                p_file.write(db_password)
+        except Exception as e:
+            logging.exception("Exception while writing the secrets files: ", e)
+
+    def _create_config_command(self, config):
+        command = ""
+        server_configuration_files = config.get_pg_config_files()
+        if not server_configuration_files:
+            return
+        for conf_file in server_configuration_files.keys():
+            command = command + " -c {}={}".format(SUPPORTED_CONFIGURATION_FILES[conf_file], f"{CUSTOM_FILES}/{conf_file}")
+        return command
+
     def _follow_container_logs(self):
         def _follow_logs():
             logs_from_container = self.postgresql_container.logs(follow=True, stream=True)
             for log in logs_from_container:
                 logging.info(log.decode())
 
-        logging.info("Following the docker container logs....")
+        logging.info(
+            "Following the docker container: {}-{} logs....".format(
+                self.postgresql_container.name, self.postgresql_container.id
+            )
+        )
         logs_thread = Thread(target=_follow_logs)
         logs_thread.start()
